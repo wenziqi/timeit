e6b7854cdc479584d1765331663e849d2e7f50ab
wenziqi <952623276@qq.com>
2020-11-27 20:28:46 +0800

    detele

--- timeit.lua
+++ timeit.lua
@@ -47,27 +47,6 @@ local function timeit(stmt,number,args)
 end
 
 
-
-
--- local function test()
--- 	local i = 0
--- 	for j=1,100 do
--- 		i = i + j
--- 	end
--- end
-
--- timeit('a=1')
--- timeit(test)
-
--- timeit('a=b+c*d\n print(a)',1,{env={b=10,c=2,d=5}})
-
--- local function test_1(n)
--- 	return n*2
--- end
-
--- timeit(test_1,nil,{func_args={10}})
--- timeit(test_1,1,{func_args={10}})
-
 return {
 	timeit=timeit,
 }
